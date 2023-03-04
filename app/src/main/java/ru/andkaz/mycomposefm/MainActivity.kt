f1f99f2a3eeeec332d91232d12c5df4aaf40b682
AndrewPod1968 <and230620@yandex.ru>
2023-03-04 15:04:14 +0700

    lesson end

--- app/src/main/java/ru/andkaz/mycomposefm/MainActivity.kt
+++ app/src/main/java/ru/andkaz/mycomposefm/MainActivity.kt
@@ -3,12 +3,12 @@ package ru.andkaz.mycomposefm
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -34,7 +34,36 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Test git lesson1",Modifier.padding(30.dp), fontSize = 28.sp)
+   //Card(modifier = Modifier.fillMaxWidth())
+    val message = remember{mutableStateOf("")}
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    )
+   {
+  /*     Text(
+           text = "Test git lesson1",
+//           Modifier
+//               .padding(30.dp)
+               //.offset(x = 20.dp, y = 30.dp),
+           //fontSize = 28.sp
+       )*/
+       Text(text = "12324")
+       Button(onClick = { /*TODO*/ }, modifier = Modifier.fillMaxWidth().padding(10.dp)
+       ) {
+           Text("Get")
+       }
+       TextField(value = message.value, onValueChange ={message.value=it} )
+       Button(onClick = { /*TODO*/ }, modifier = Modifier.fillMaxWidth().padding(10.dp)
+       ) {
+           Text("Save")
+       }
+       //OutlinedTextField(value = "Hello",){
+
+
+       //Text(text = "12324")
+
+   }
 }
 
 @Preview(showBackground = true)
